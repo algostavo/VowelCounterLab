2be32354acf94a054f09ecc418f208f4a91eb2db
steve <algostavo@gmail.com>
2019-02-24 01:19:46 -0500

    Letter case check.

--- VowelCounterLab/VowelCounterLab.cpp
+++ VowelCounterLab/VowelCounterLab.cpp
@@ -11,6 +11,9 @@ Gives the user the option to restart the program
 #include "pch.h"
 #include <iostream>
 #include <string>
+#include <locale>
+#include <functional>
+#include <algorithm>
 
 char vowel_bank[6] = { 'a','e','i','o','u','\0' };
 
@@ -38,9 +41,11 @@ std::string output_result(const int vowel_count, char* c_string)
 
 int main()
 {
+	const std::locale loc;
 	char c_string[128];
-	char input_string[128];
+	char input_line[128];
 	char restart_response[2];
+	typedef unsigned char byte;
 
 	while (std::strncmp(restart_response, "Q", 1) != 0)
 	{
@@ -48,11 +53,17 @@ int main()
 		const auto restart = "\n\n* Do you want to restart the program? *\n* Any key to continue or 'Q' to quit  *\n";
 
 		std::cout << menu;
-		std::cin.getline(input_string, 128);
-		strcpy_s(c_string, input_string);
+		std::cin.getline(input_line, 128);
+
+		auto input_string = std::string(input_line);
+		std::transform(input_string.begin(), input_string.end(), input_string.begin(),
+            static_cast<std::function<int(byte)>>(::tolower));
+
+		const auto input_char = input_string.c_str();
+		strcpy_s(c_string, input_char);
 
 		const auto vowel_count = vowel_counter(c_string, vowel_bank);
-		const auto menu_output = output_result(vowel_count, c_string);
+		const auto menu_output = output_result(vowel_count, input_line);
 
 		std::cout << menu_output;
 		std::cout << restart;
